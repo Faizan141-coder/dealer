96f2dab9be219af49f786b1cc4e02960ad2d3820
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2024-08-27 20:56:14 +0500

    installed next js

--- frontend/src/app/api/auth/[...nextauth]/pages-options.ts
+++
@@ -1,5 +0,0 @@
-import { PagesOptions } from "next-auth";
-
-export const pagesOptions: Partial<PagesOptions> = {
-    signIn: "/login",
-};
--- mobile/android/app/src/main/kotlin/com/aridoosti/app/mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.aridoosti.app.mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
