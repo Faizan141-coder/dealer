ef2fc13c2bbfe573a8f46f1eee371a34ece12c7f
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2024-08-27 21:57:30 +0500

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -12,6 +12,8 @@
 //         <div className="mx-auto grid w-[360px] gap-6">
 //           <div className="grid gap-2 text-center">
 //             <h1 className="text-3xl font-bold">Login</h1>
+//             <p className="text-balance text-muted-foreground">
+//               Enter your email below to login to your account
 //             </p>
 //           </div>
 //           <div className="grid gap-4">
